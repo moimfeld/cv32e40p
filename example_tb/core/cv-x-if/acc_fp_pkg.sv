e4f4759ca3c53d80254049c165e493090b91b551
Moritz Imfeld <moimfeld@student.ethz.ch>
2021-05-22 11:04:23 +0200

    Add missing F extension instruction
    xfvec instructions are still missing
    
    Signed-off-by: Moritz Imfeld <moimfeld@student.ethz.ch>

--- example_tb/core/cv-x-if/acc_fp_pkg.sv
+++ example_tb/core/cv-x-if/acc_fp_pkg.sv
@@ -14,8 +14,28 @@
 package acc_fp_pkg;
 
 // parameter int unsigned NumInstr="NUMBER OF FP INSTR";
-parameter int unsigned NumInstr=8;
-parameter acc_pkg::offload_instr_t OffloadInstr[8] = '{
+parameter int unsigned NumInstr=26;
+parameter acc_pkg::offload_instr_t OffloadInstr[26] = '{
+  '{
+    instr_data: 32'b 000000000000_00000_010_00000_0000111, // FLW
+    instr_mask: 32'b 000000000000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b1,
+      p_use_rs : 3'b001
+   }
+  },
+  '{
+    instr_data: 32'b 0000000_00000_00000_010_00000_0000111, // FSW
+    instr_mask: 32'b 0000000_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b1,
+      p_use_rs : 3'b001
+   }
+  },
   '{
     instr_data: 32'b 00000_00_00000_00000_000_00000_1000011, // FMADD.S
     instr_mask: 32'b 00000_11_00000_00000_000_00000_1111111,
@@ -87,13 +107,173 @@ parameter acc_pkg::offload_instr_t OffloadInstr[8] = '{
     }
   },
   '{
-    instr_data: 32'b 0001100_00000_00000_000_00000_1001011, // FDIV.S
+    instr_data: 32'b 0001100_00000_00000_000_00000_1010011, // FDIV.S
     instr_mask: 32'b 1111111_00000_00000_000_00000_1111111,
     prd_rsp : '{
       p_accept : 1'b1,
       p_writeback : 2'b00,
       p_is_mem_op : 1'b0,
       p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0101100_00000_00000_000_00000_1010011, // FSQRT.S
+    instr_mask: 32'b 1111111_11111_00000_000_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0010000_00000_00000_000_00000_1010011, // FSGNJ.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0010000_00000_00000_001_00000_1010011, // FSGNJN.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0010000_00000_00000_010_00000_1010011, // FSGNJX.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0010100_00000_00000_000_00000_1010011, // FMIN.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 0010100_00000_00000_001_00000_1010011, // FMAX.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1100000_00000_00000_000_00000_1010011, // FCVT.W.S
+    instr_mask: 32'b 1111111_11111_00000_000_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1100000_00001_00000_000_00000_1010011, // FCVT.WU.S
+    instr_mask: 32'b 1111111_11111_00000_000_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1110000_00000_00000_000_00000_1010011, // FMV.X.W
+    instr_mask: 32'b 1111111_11111_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1010000_00000_00000_010_00000_1010011, // FEQ.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1010000_00000_00000_001_00000_1010011, // FLT.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1010000_00000_00000_000_00000_1010011, // FLE.S
+    instr_mask: 32'b 1111111_00000_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1110000_00000_00000_001_00000_1010011, // FCLASS.S
+    instr_mask: 32'b 1111111_11111_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b01,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b000
+    }
+  },
+  '{
+    instr_data: 32'b 1101000_00000_00000_000_00000_1010011, // FCVT.S.W
+    instr_mask: 32'b 1111111_11111_00000_000_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b001
+    }
+  },
+  '{
+    instr_data: 32'b 1101000_00001_00000_000_00000_1010011, // FCVT.S.WU
+    instr_mask: 32'b 1111111_11111_00000_000_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b001
+    }
+  },
+  '{
+    instr_data: 32'b 1111000_00000_00000_000_00000_1010011, // FMV.W.X
+    instr_mask: 32'b 1111111_11111_00000_111_00000_1111111,
+    prd_rsp : '{
+      p_accept : 1'b1,
+      p_writeback : 2'b00,
+      p_is_mem_op : 1'b0,
+      p_use_rs : 3'b001
     }
   }
 };
