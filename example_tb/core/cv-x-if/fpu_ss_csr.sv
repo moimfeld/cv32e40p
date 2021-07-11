87b82c0df21e5df6fce1da0219943f7b4ac9e3c2
Moritz Imfeld <moimfeld@student.ethz.ch>
2021-07-11 10:12:21 +0200

    Add CSR update after fp instruction completes
    Signed-off-by: Moritz Imfeld <moimfeld@student.ethz.ch>

--- example_tb/core/cv-x-if/fpu_ss_csr.sv
+++ example_tb/core/cv-x-if/fpu_ss_csr.sv
@@ -15,12 +15,14 @@ module fpu_ss_csr (
     input  logic clk_i,
     input  logic rst_ni,
 
-    input  logic [31:0] instr_i,
-    input  logic [31:0] csr_data_i,
-    output logic [31:0] csr_rdata_o,
-    output logic [ 2:0] frm_o,
-    output logic        csr_wb_o,
-    output logic        csr_instr_o
+    input  logic [31:0]        instr_i,
+    input  logic [31:0]        csr_data_i,
+    input  fpnew_pkg::status_t fpu_status_i,
+    input  logic               fpu_out_valid_i,
+    output logic [31:0]        csr_rdata_o,
+    output logic [ 2:0]        frm_o,
+    output logic               csr_wb_o,
+    output logic               csr_instr_o
 
     );
 
@@ -33,47 +35,54 @@ module fpu_ss_csr (
       csr_wb_o    = 1'b0;
       csr_rdata_o = '0;
       csr_instr_o = 1'b1;
-      unique casez (instr_i)
-        fpu_ss_instr_pkg::CSRRW_FSCSR: begin // Swap value in fcsr with the one in rs1
-          fcsr_d      = csr_data_i;
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = fcsr_q;
-        end
-        fpu_ss_instr_pkg::CSRRS_FRCSR: begin // Read value from fcsr and copy to int reg
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = fcsr_q;
-        end
-        fpu_ss_instr_pkg::CSRRW_FSRM: begin // Swap frm value in fcsr with the one in rs1
-          fcsr_d[7:5] = csr_data_i[2:0];
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = {29'b0, fcsr_q[7:5]};
-        end
-        fpu_ss_instr_pkg::CSRRS_FRRM: begin // Read frm from fcsr and copy to int reg (zeropadding at the front)
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = {29'b0, fcsr_q[7:5]};
-        end
-        fpu_ss_instr_pkg::CSRRWI_FSRMI: begin // Swap frm value in fcsr with the one in the immediat instr_i [17:15] (immediat is at [19:15])
-          fcsr_d[7:5] = instr_i[17:15];
-        end
-        fpu_ss_instr_pkg::CSRRW_FSFLAGS: begin // Swap fflags value in fcsr with the one in rs1
-          fcsr_d[4:0] = csr_data_i[4:0];
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = {27'b0, fcsr_q[4:0]};
-        end
-        fpu_ss_instr_pkg::CSRRS_FRFLAGS: begin // Read fflags from fcsr and copy to int reg (zeropadding at the front)
-          csr_wb_o    = 1'b1;
-          csr_rdata_o = {27'b0, fcsr_q[4:0]};
-        end
-        fpu_ss_instr_pkg::CSRRWI_FSFLAGSI: begin // Swap frm value in fcsr with the one in the immediat instr_i [19:15] (immediat is at [19:15])
-          fcsr_d[4:0] = instr_i[19:15];
-        end
-        default: begin
-          fcsr_d      = fcsr_q;
-          csr_wb_o    = 1'b0;
-          csr_rdata_o = '0;
-          csr_instr_o = 1'b0;
-        end
-      endcase
+      if(~fpu_out_valid_i) begin
+        unique casez (instr_i)
+          fpu_ss_instr_pkg::CSRRW_FSCSR: begin // Swap value in fcsr with the one in rs1
+            fcsr_d      = csr_data_i;
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = fcsr_q;
+          end
+          fpu_ss_instr_pkg::CSRRS_FRCSR: begin // Read value from fcsr and copy to int reg
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = fcsr_q;
+          end
+          fpu_ss_instr_pkg::CSRRW_FSRM: begin // Swap frm value in fcsr with the one in rs1
+            fcsr_d[7:5] = csr_data_i[2:0];
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = {29'b0, fcsr_q[7:5]};
+          end
+          fpu_ss_instr_pkg::CSRRS_FRRM: begin // Read frm from fcsr and copy to int reg (zeropadding at the front)
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = {29'b0, fcsr_q[7:5]};
+          end
+          fpu_ss_instr_pkg::CSRRWI_FSRMI: begin // Swap frm value in fcsr with the one in the immediat instr_i [17:15] (immediat is at [19:15])
+            fcsr_d[7:5] = instr_i[17:15];
+          end
+          fpu_ss_instr_pkg::CSRRW_FSFLAGS: begin // Swap fflags value in fcsr with the one in rs1
+            fcsr_d[4:0] = csr_data_i[4:0];
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = {27'b0, fcsr_q[4:0]};
+          end
+          fpu_ss_instr_pkg::CSRRS_FRFLAGS: begin // Read fflags from fcsr and copy to int reg (zeropadding at the front)
+            csr_wb_o    = 1'b1;
+            csr_rdata_o = {27'b0, fcsr_q[4:0]};
+          end
+          fpu_ss_instr_pkg::CSRRWI_FSFLAGSI: begin // Swap frm value in fcsr with the one in the immediat instr_i [19:15] (immediat is at [19:15])
+            fcsr_d[4:0] = instr_i[19:15];
+          end
+          default: begin
+            fcsr_d      = fcsr_q;
+            csr_wb_o    = 1'b0;
+            csr_rdata_o = '0;
+            csr_instr_o = 1'b0;
+          end
+        endcase
+      end else begin
+        fcsr_d      = {fcsr_q[31:5], fpu_status_i.NV, fpu_status_i.DZ, fpu_status_i.OF, fpu_status_i.UF, fpu_status_i.NX};
+        csr_wb_o    = 1'b0;
+        csr_rdata_o = '0;
+        csr_instr_o = 1'b0;
+      end
     end
 
     always_ff @(posedge clk_i,negedge rst_ni) begin
